4b5895f4f346675b5e5654e69d80f3e5f7056fe4
Nikitashukla01 <91081299+Nikitashukla01@users.noreply.github.com>
2021-09-20 21:40:47 +0530

    Add files via upload

---
+++ 33.py
@@ -0,0 +1,31 @@
+class student:
+    count = 0
+    def __inti__(self,name):
+        
+        self.name=name
+        
+        self.marks={}
+          
+        student.count =students.count +1
+        
+                
+    def entermarks (self):
+        for i in range (3):
+            m = int (input("enter the marks of %s in %d subject: "%(self.name, i+1)))
+            
+    self.marks.append(m)
+                           
+def display(self):
+                               print (self.name, "got", self.marks)
+                               name =input ("enter the name of the student :")
+                               s1=students (name)
+                               s1.entermarks()
+                               s1.display()
+                               print ("")
+                               name =input("enter the name of the student :")
+                               
+s2=students(name)
+s2.entermarks()
+s2.display()
+
+s2.displaycount()
